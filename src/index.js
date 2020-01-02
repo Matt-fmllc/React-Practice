19c1c165daeb0367a98fa2f5154822f772756afc
Matt-fmllc <matt.fmllc@gmail.com>
2020-01-01 17:33:07 -0800

    more forms practice

---
+++ src/index.js
@@ -0,0 +1,67 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+/* just adding conditoinal rendering
+ * makes no sense if not going to show the 
+ * number of guests box
+ */
+
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+
+    }
+
+
+    render() {
+        return (
+            <form >
+                <label>
+                    is going:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange}   />
+                </label>
+                <br />
+                <label>
+                    Number of guests:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+         );
+    }
+                
+}
+ 
+
+ReactDOM.render(
+    <Reservation />,
+    document.getElementById('root')
+);
+
+
